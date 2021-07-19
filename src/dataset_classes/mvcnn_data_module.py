4a05f6f0f2629a30467ed46796b01b7ebfdf9004
TomaszKaleczyc <tomasz.kaleczyc@gmail.com>
2021-07-19 17:41:01 +0200

    pl module created

--- src/dataset_classes/mvcnn_data_module.py
+++ src/dataset_classes/mvcnn_data_module.py
@@ -1,21 +1,26 @@
 from torch.utils.data import DataLoader
 from pytorch_lightning import LightningDataModule
 
+from dataset_classes.mvcnn_dataset import MVCNNDataset
+from utilities import consts
+
 
 class MVCNNDataModule(LightningDataModule):
     """
-    
+    Manages the model datasets
     """
 
-    def __init__(self):
+    def __init__(self, batch_size=1):
         super().__init__()
-        pass
+        self._batch_size = batch_size
+        self._train_dataset = MVCNNDataset(consts.TRAIN_FOLDER_NAME)
+        self._val_dataset = MVCNNDataset(consts.VALIDATION_FOLDER_NAME)
 
     def train_dataloader(self):
-        pass
-
-    def test_dataloader(self):
-        pass
+        return DataLoader(self._train_dataset, batch_size=self._batch_size, shuffle=True)
 
     def val_dataloader(self):
-        pass
+        return DataLoader(self._val_dataset, batch_size=self._batch_size, shuffle=False)
+
+    def test_dataloader(self):
+        raise NotImplementedError
